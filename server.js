82c3bb65a397ab8d04584785301267aaed0722e5
Nitin K <nitink61996@gmail.com>
2022-07-13 13:04:29 +0530

    using req.params

--- server.js
+++ server.js
@@ -4,12 +4,37 @@ const app = express();
 
 const PORT = 2020;
 
+const persons = [
+    {
+        id: 1,
+        name: 'ABC',
+    },
+    {
+        id: 2,
+        name: 'XYZ',
+    },
+];
+
 app.get('/', (req, res) => {
     res.send(' You have hit the main route :) ');
 });
 
 app.get('/test', (req, res) => {
     res.send('<ul><li>Hey Test !</li></ul>');
+});
+
+app.get('/persons', (req, res) => {
+    res.json(persons);
+});
+
+app.get('/persons/:personID', (req, res) => {
+    const personID = req.params.personID;
+    const result = persons[personID];
+    if (result) {
+        res.json(result);
+    } else {
+        res.status(400).json({ error: 'NOt FOund ' });
+    }
 });
 
 app.listen(PORT, () => {
